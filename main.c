ad7d5fe7bd354d2b3a9d4ae8f2dd31eb2ab61221
ELHADRINadia <93933520+ELHADRINadia@users.noreply.github.com>
2021-11-18 18:20:12 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,50 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include<stdbool.h>
+
+
+void vote1(char cinElection[8][8])
+    {
+        char namePresident[20][20];
+        int nbrPresident, nbrElection, i, vote;
+        do
+        {
+           //choice of electors
+        printf("Cin elector choices the number of president %d :\n ",namePresident[i]);
+        scanf("%d",&vote);
+        if(vote>0 && vote<=nbrPresident){
+						    	vote+=1;
+						    	i++;
+							}else{
+						        printf("(Please ! repeate your vote) ");
+						         }
+        }
+        while(i<=nbrPresident);
+
+    }
 
 int main()
 {
-    int N1 ,N2 ;
-    bool a;
-    char choix ;
-    printf("saisir le nombre a\n");
-    scanf("%d",&a);
-    if(a==1){
-    printf("saisir le nombre N1\n");
-    scanf("%d",&N1);
-    printf("saisir l'opérateur de choix\n");
-    scanf("%S",&choix);
-    printf("saisir le nombre N2\n");
-    scanf("%d",&N2);
-    }
-    else if(a==0){
-        printf("off");
-    }
-    else{
-        printf("error");
-    }
-    switch(choix)
-    {
-    case '+':
-    printf ("%d + %d = %d\n",N1,N2,N1+N2);
-    break;
-    case '-':
-    printf ("%d - %d = %d\n",N1,N2,N1-N2);
-    break;
-    case '*':
-    printf ("%d * %d = %d\n",N1,N2,N1*N2);
-    break;
-    case '/':
-        if(N2 !=0){
-    printf ("%d / %d = %.2f\n",N1,N2,(float)N1/N2);
-    }
-    else {
-        printf("Error");
-    }
-    break;
-    default : printf("operation inconnue");
+    int nbrPresident,nbrElection,nbrVote,i,j,vote;
+    char namePresident[20][20],cinElection[8][8];
+    //started to coding
+    printf("*************** Election presidentielle *************** \n\n");
+    printf("Welcome Mr/M in my new application \n \n");
+    //check number of presidents
+    do
+        {
+          printf("Please enter the number of president ! \n");
+          scanf("%d",&nbrPresident);
+        }
+    while(nbrPresident<5);
 
+    // name of presidents
+   for(i=1;i<= nbrPresident; i++){
+        printf("enter name of president :\n");
+        scanf("%s",namePresident[i]);
     }
+    //check number of Electors
+    do
+        {
+          printf("Please enter the number of electors! \n");
+          scanf("%d",&nbrElection);
+        }
+    while(nbrElection<10);
+    // cin of electors
+   for(i=1;i<= nbrElection; i++){
+        printf("enter the cin of elector : \n");
+        scanf("%s",cinElection[i]);
+    }
+    for(j = 1;j<=nbrPresident;j++)
+        {
+        printf ("%d- the president N %d est : %s\n",j,j,namePresident[j]);
+        }
+
+    // tower number 1
+   vote1(cinElection[8][8]);
+
     return 0;
 }
+
